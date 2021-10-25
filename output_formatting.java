a134d627cf490d206d8cf48d843691c4704f16b8
Rishi Rajpal <61634330+rishirajpal01@users.noreply.github.com>
2021-10-25 13:29:35 +0530

    Create output_formatting.java

---
+++ output_formatting.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Solution {
+
+    public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		System.out.println("================================");
+		for(int i=0;i<3;i++){
+			String s1=sc.next();
+			int x=sc.nextInt();
+			System.out.printf("%-15s%03d\n", s1, x);     //Notice the printf, %s and %d. "-" for left justification and 15 as given condition. "0" for filling space and "3" for 3 digit requirement.
+		}
+		System.out.println("================================")
+		}
+}
