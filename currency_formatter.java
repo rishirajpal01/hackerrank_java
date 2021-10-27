a5627b35dfafbe1ee38c706498362cf4b5f7a84d
Rishi Rajpal <61634330+rishirajpal01@users.noreply.github.com>
2021-10-27 18:44:06 +0530

    Create currency_formatter.java

---
+++ currency_formatter.java
@@ -0,0 +1,23 @@
+import java.util.Locale;
+import java.util.Scanner;
+import java.text.NumberFormat;
+public class currency_formatter {
+    
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        double payment = scanner.nextDouble();
+        scanner.close();
+        
+        Locale indiaLocale = new Locale("en", "IN");   // Creating Locale for India.
+        
+        NumberFormat us     = NumberFormat.getCurrencyInstance(Locale.US);  //Formatting for each country.
+        NumberFormat india  = NumberFormat.getCurrencyInstance(indiaLocale);
+        NumberFormat china  = NumberFormat.getCurrencyInstance(Locale.CHINA);
+        NumberFormat france = NumberFormat.getCurrencyInstance(Locale.FRANCE);
+        
+        System.out.println("US: " + us.format(payment));
+        System.out.println("India: " + india.format(payment));
+        System.out.println("China: " + china.format(payment));
+        System.out.println("France: " + france.format(payment));
+    }
+}
